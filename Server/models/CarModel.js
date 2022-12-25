a7b5291fbd9c6a8d9ed367151b0240a7544cb561
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-12-25 19:18:07 +0200

    Car Model Update

--- Server/models/CarModel.js
+++ Server/models/CarModel.js
@@ -1,23 +1,18 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
 
 const CarScheme = new Schema({
-  Brand: String,
-  Model: String,
-  Mileage: String,
-  Colour: String,
-  Price: String,
-  EngineVolume: String,
-  TypeOfDrive: String,
-  GearboxType: String,
-  YearOfIssue: String,
-  FuelType: String,
-  LinkToPicture:String,
+    Brand: { type: String, required: true },
+    Model: { type: String, required: true },
+    Mileage: { type: String, required: true },
+    Colour: { type: String, required: true },
+    Price: { type: String, required: true },
+    EngineVolume: { type: String, required: true },
+    TypeOfDrive: { type: String, required: true },
+    GearboxType: { type: String, required: true },
+    YearOfIssue: { type: String, required: true },
+    FuelType: { type: String, required: true },
+    LinkToPicture: { type: String, required: true },
 });
 
-const Car = mongoose.model("cars", CarScheme);
-
-module.exports = {
-  Car,
-};
+module.exports = model('Car', CarScheme);
 
