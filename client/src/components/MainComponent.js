ff205d3c46723e7c55bbddbe9826e50ea298f4ba
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-12-26 06:55:43 +0200

    Some dizigne Update

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -24,6 +24,7 @@ function CarFetch(props) {
     }
     else if (car == null || car?.length <= 0) {
         return <div>
+             <a href="http://localhost:3000/addcard" class="floating-button">Add Card</a>
             <div>There is no Cars</div>
             <div onClick={getCar} className={'button'}>Update</div>
         </div>
@@ -35,8 +36,8 @@ function CarFetch(props) {
             {car?.map((cursor) => {
                 return <div className="card">
                     <img className="cardimg" src={cursor.LinkToPicture}></img>
-                    <h4>{cursor.Brand} {cursor.Model} {cursor.YearOfIssue}</h4>
-                    <h5>{cursor.Mileage}</h5>
+                    <h4>Марка:{cursor.Brand} Модель:{cursor.Model} Год выпуска:{cursor.YearOfIssue}</h4>
+                    <h5>Пробег:{cursor.Mileage}</h5>
                     <h5>{cursor.TypeOfDrive}{cursor.EngineVolume}</h5>
                     <h5>{cursor.GearboxType}{cursor.FuelType}</h5>
                     <h5>{cursor.Price}</h5>
